8a3eb2c16f12de72b5c02a5528f6b366d66f9ec5
JonathanMurray <jonte_murray@hotmail.com>
2015-07-06 23:17:12 +0200

    ser id

--- app/models/Person.java
+++ app/models/Person.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
  * Created by jonathan on 2015-07-06.
  */
 public class Person implements Serializable {
+    public static final long serialVersionUID = 0;
     public final String name;
     public final int age;
 
